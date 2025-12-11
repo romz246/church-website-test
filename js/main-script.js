ac3c2d24c8f7443c3ac794f4cfe1174dce419485
romz246 <romarioclarke5@gmail.com>
2025-12-11 10:09:26 -0400

    init commit

---
+++ js/main-script.js
@@ -0,0 +1,93 @@
+// Automatically hightlights the current page
+document.addEventListener("DOMContentLoaded", () => {
+	const navLinks = document.querySelectorAll(".nav-links li a");
+	const currentURL = window.location.href;
+
+	navLinks.forEach((link) => {
+		link.removeAttribute("aria-current");
+		if (link.href === currentURL) {
+			link.setAttribute("aria-current", "page");
+		}
+	});
+});
+
+// Dark Mode Toggle
+
+function darkModeToggle() {
+	const toggleBtn = document.getElementById("theme-toggle");
+
+	// Toggle theme on click
+	toggleBtn.addEventListener("click", () => {
+		const current = document.documentElement.getAttribute("data-theme");
+		const newTheme = current === "dark" ? "light" : "dark";
+		document.documentElement.setAttribute("data-theme", newTheme);
+		localStorage.setItem("theme", newTheme);
+	});
+
+	// Auto update if system preference changes (no user override)
+	window
+		.matchMedia("(prefers-color-scheme: dark)")
+		.addEventListener("change", () => {
+			if (!localStorage.getItem("theme")) {
+				const systemDark = window.matchMedia(
+					"(prefers-color-scheme: dark)",
+				).matches;
+				document.documentElement.setAttribute(
+					"data-theme",
+					systemDark ? "dark" : "light",
+				);
+			}
+		});
+}
+
+darkModeToggle();
+
+function hamburgerMneu() {
+	const hamburgerMenuBtn = document.getElementById("hamburger-menu-btn");
+
+	const navLinks = document.getElementById("nav-links");
+
+	hamburgerMenuBtn.addEventListener("click", () => {
+		const currentState = hamburgerMenuBtn.getAttribute("data-state");
+
+		if (!currentState || currentState === "closed") {
+			hamburgerMenuBtn.setAttribute("data-state", "opened");
+			hamburgerMenuBtn.setAttribute("aria-expanded", "true");
+			navLinks.classList.toggle("open");
+		} else {
+			hamburgerMenuBtn.setAttribute("data-state", "closed");
+			hamburgerMenuBtn.setAttribute("aria-expanded", "false");
+			navLinks.classList.remove("open");
+		}
+	});
+}
+
+hamburgerMneu();
+
+function copyrightYear() {
+	const yearSpan = document.getElementById("year");
+	const startYear = 2024;
+
+	// Set current text
+	const currentYear = new Date().getFullYear();
+	yearSpan.textContent =
+		startYear === currentYear
+			? `${startYear}`
+			: `${startYear} - ${currentYear}`;
+
+	// Calculate milliseconds until next year
+	const present = new Date();
+	const nextYear = new Date(currentYear + 1, 0, 1);
+	const msUntilNextYear = nextYear - present;
+
+	// Schedule update at midnight Jan 1
+	setTimeout(() => {
+		const newYear = new Date().getFullYear();
+		yearSpan.textContent =
+			startYear === newYear ? `${startYear}` : `${startYear} - ${newYear}`;
+		// Schedule the next update recursively
+		copyrightYear();
+	}, msUntilNextYear);
+}
+
+copyrightYear();
